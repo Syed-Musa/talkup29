8584a8b12e6938493c7638b0701480f249a7edf1
Syed Mohd Musa <syedmohdmusa2002@gmail.com>
2025-12-13 22:41:31 +0530

    Change baseURL to production ML service domain

--- Frontend/src/lib/mlAxios.js
+++ Frontend/src/lib/mlAxios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const mlAxios = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://YOUR_ML_SERVICE_DOMAIN.onrender.com",
   timeout: 5000,
 });
 
 export default mlAxios;
+
