46a5a6aa632e3f96503f8f6e71b08d732938817b
Syed Mohd Musa <syedmohdmusa2002@gmail.com>
2025-12-13 18:05:03 +0530

    Update userToken.js

--- Backend/src/controllers/userToken.js
+++ Backend/src/controllers/userToken.js
@@ -3,10 +3,11 @@ export const generateUserToken = async (res, _id) => {
   const userToken = jwt.sign({ _id:_id }, process.env.JWT_SECRET , {
     expiresIn: "7d",
   });
-  res.cookie("userToken", userToken, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: "lax",
-    secure: process.env.NODE_ENV !== "development",
-  });
+res.cookie("userToken", userToken, {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",
+  secure: true,
+});
+
 };
